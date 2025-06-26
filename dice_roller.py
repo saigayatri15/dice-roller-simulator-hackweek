c3c4def38be83124ea7ac755a96f2c00ba16d7f6
saigayatri15 <sai15gayatri@gmail.com>
2025-06-26 22:44:10 +0530

    Added python script and output video

---
+++ dice_roller.py
@@ -0,0 +1,82 @@
+import random
+import time
+import os
+dice_art={
+    1:(
+"┌─────────┐",
+"│         │",
+"│    ●    │",
+"│         │",
+"└─────────┘"
+    ),
+    2:(
+"┌─────────┐",
+"│  ●      │",
+"│         │",
+"│      ●  │",
+"└─────────┘"
+    ),
+    3:(
+"┌─────────┐",
+"│  ●      │",
+"│    ●    │",
+"│      ●  │",
+"└─────────┘"
+    ),
+    4:(
+"┌─────────┐",
+"│  ●   ●  │",
+"│         │",
+"│  ●   ●  │",
+"└─────────┘"
+    ),
+    5:(
+"┌─────────┐",
+"│  ●   ●  │",
+"│    ●    │",
+"│  ●   ●  │",
+"└─────────┘"
+    ),
+    6:(
+"┌─────────┐",
+"│  ●   ●  │",
+"│  ●   ●  │",
+"│  ●   ●  │",
+"└─────────┘"
+    )
+}
+
+
+def clear_screen():
+    """Clears the terminal screen based on the operating system."""
+    if os.name == 'nt':  # For Windows
+        os.system('cls')
+    else:  # For Unix-based systems (Linux, macOS)
+        os.system('clear')
+
+# Call the function to clear the screen
+clear_screen()
+
+def roll_dice():
+    result=random.randint(1,6)
+    clear_screen()
+    for _ in range(20):
+        for line in dice_art[random.randint(1,6)]:
+            print(line)
+        time.sleep(0.1)
+        clear_screen()
+    print("You have rolled:")
+    for line in dice_art[result]:
+        print(line)
+
+
+def main():
+    while True:
+        play=input("Press the Enter key to roll the die: ")
+        if play=="":
+            roll_dice()
+            break
+        else:
+            print("Invalid input entered. Try again.")
+
+main()
